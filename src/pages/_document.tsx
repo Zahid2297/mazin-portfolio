e6f2f744846e0347ca35ad3a10af5e4fde92bb89
Zahid2297 <techpirate97@gmail.com>
2026-06-22 23:53:59 +0530

    update

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,7 +3,9 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <link rel="icon" href="/static/Bodyfb.png" />
+      </Head>
       <body className="bg-background text-foreground antialiased selection:bg-accent selection:text-background">
         <Main />
         <NextScript />
